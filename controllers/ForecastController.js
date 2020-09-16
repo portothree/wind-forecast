3eb433df5bf3acda94d13974e8dca58128420956
Gustavo Porto <gporto@quanticosolutions.com>
2020-09-16 16:56:41 +0100

    :construction: getting data from json file in tmp folder

--- controllers/ForecastController.js
+++ controllers/ForecastController.js
@@ -1,19 +1,13 @@
 const axios = require('axios');
-const noaa = require('../configs/noaa');
+
+const data = require('../tmp/wind-global.json');
 
 module.exports = {
 	async getLatest(req, res, next) {
 		try {
-			const data = [];
-
-			const response = await axios.get(noaa.baseDir, {
-				params: noaa.qs,
-			});
-
 			res.status(200);
 			return res.json(data);
 		} catch (error) {
-			console.log(error);
 			next(error);
 		}
 	},
