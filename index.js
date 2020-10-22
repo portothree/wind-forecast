f2a3bb4f8d900eff1678f83b1868e2fe4fbba63a
Gustavo Porto <gporto@quanticosolutions.com>
2020-10-22 10:20:35 +0100

    :recycle: implement node-cron to download and convert grib2 data to json

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const { getLatestGrib } = require('./utils.js');
+require('dotenv').config();
+const cron = require('node-cron');
 
-function main() {
-	const gribPath = `${process.env.TMP_PATH}/latest.grib2`;
+const { getLatestGrib, convertGribToJson } = require('./utils.js');
+const gribPath = `${process.env.TMP_PATH}/latest.grib2`;
 
+// Download new GFS data from NCEP in grib2 format
+cron.schedule('0 */5 * * *', () => {
 	const now = new Date();
 	const year = now.getFullYear();
 	const month = now.getMonth() + 1;
 	const day = now.getDate();
 
-	getLatestGrib(gribPath, `${year}${month}${day}`, '00');
-}
+	try {
+		getLatestGrib(gribPath, `${year}${month}${day}`, '00');
+	} catch (error) {
+		process.exit(1);
+	}
+});
 
-main();
+// Convert grib2 to json
+cron.schedule('0 */6 * * *', () => {
+	try {
+		convertGribToJson(gribPath, `${process.env.TMP_PATH}/wind-global.json`);
+	} catch (error) {
+		process.exit(1);
+	}
+});
